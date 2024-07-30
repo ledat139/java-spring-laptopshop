9a181ef747b72ba2cc1fc49aeffb64a74f1861cf
ledat139 <tiendatcntt1307@gmail.com>
2024-07-30 21:07:21 +0700

    table relationships

--- src/main/java/vn/tdat/laptopshop/controller/UserController.java
+++ src/main/java/vn/tdat/laptopshop/controller/UserController.java
@@ -86,5 +86,7 @@ public class UserController {
         this.userService.deleteUser(TDat.getId());
         return "redirect:/admin/user";
     }
+    
+    
 
 }
--- src/main/java/vn/tdat/laptopshop/domain/User.java
+++ src/main/java/vn/tdat/laptopshop/domain/User.java
@@ -1,11 +1,18 @@
 package vn.tdat.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "users")
 public class User {
 
     @Id
@@ -16,7 +23,16 @@ public class User {
     private String fullName;
     private String address;
     private String phone;
+    private String avatar;
 
+    // Role id
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
+    
     public long getId() {
         return id;
     }
@@ -65,10 +81,18 @@ public class User {
         this.phone = phone;
     }
 
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
     @Override
     public String toString() {
         return "User [id=" + id + ", email=" + email + ", password=" + password + ", fullName=" + fullName
-                + ", address=" + address + ", phone=" + phone + "]";
+                + ", address=" + address + ", phone=" + phone + ", avatar=" + avatar + "]";
     }
 
 }
